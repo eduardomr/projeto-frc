6ef338710237a9a7a9142dffe4c230b2cab75c40
ArthurFerreiraRodrigues <arthur.250402@gmail.com>
2023-12-20 22:04:14 -0300

    Restringe opções de chat

--- front/conversify/src/app/conversify/chat/pages/select-chat-type/select-chat-type.component.ts
+++ front/conversify/src/app/conversify/chat/pages/select-chat-type/select-chat-type.component.ts
@@ -1,25 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import { NavigationService } from 'src/app/shared/services/navigation.service';
 
+import { UserService } from './../../../login/services/user.service';
+
 @Component({
   selector: 'select-chat-type',
   templateUrl: './select-chat-type.component.html',
   styleUrls: ['./select-chat-type.component.scss'],
 })
 export class SelectChatTypeComponent implements OnInit {
-  constructor(private navigationService: NavigationService) {}
+  constructor(
+    private navigationService: NavigationService,
+    private userService: UserService
+  ) {}
 
   ngOnInit(): void {}
 
+  getUser(navRoute: string) {
+    this.userService
+      .retrieve(localStorage.getItem('token') as string)
+      .subscribe({
+        next: () => {
+          console.log('Usuário loggado');
+          this.navigationService.navigate(['chat', navRoute]);
+        },
+        error: () => {
+          alert('Realize o login para acessar o chat!');
+          this.navigationService.navigate(['access', 'login']);
+        },
+      });
+  }
+
   texto() {
-    this.navigationService.navigate(['chat', 'text']);
+    this.getUser('text');
   }
 
   video() {
-    this.navigationService.navigate(['chat', 'video']);
+    this.getUser('video');
   }
 
   ambos() {
-    this.navigationService.navigate(['chat', 'both']);
+    this.getUser('both');
   }
 }

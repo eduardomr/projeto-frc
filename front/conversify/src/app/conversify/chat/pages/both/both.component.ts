b40f7d4634e707bae606106f0b7e74fccf96ee72
ArthurFerreiraRodrigues <arthur.250402@gmail.com>
2023-12-20 21:45:14 -0300

    Video com Chat finalizado

--- front/conversify/src/app/conversify/chat/pages/both/both.component.ts
+++ front/conversify/src/app/conversify/chat/pages/both/both.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+
+import { TextComponent } from '../text/text.component';
 
 @Component({
   selector: 'both',
   templateUrl: './both.component.html',
-  styleUrls: ['./both.component.scss']
+  styleUrls: ['./both.component.scss'],
 })
 export class BothComponent implements OnInit {
+  dialogRef = this.dialog.open(TextComponent, {
+    width: '40%',
+  });
 
-  constructor() { }
+  constructor(public dialog: MatDialog) {}
 
   ngOnInit(): void {
+    this.dialogRef.close();
   }
 
+  openChat() {
+    this.dialogRef = this.dialog.open(TextComponent, {
+      width: '40%',
+    });
+    this.dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed');
+    });
+  }
 }
